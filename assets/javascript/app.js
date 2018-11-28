26b3421f5ab19ae75d31679437c085df3d0e0801
Kinan Al Shaghouri <kinanalshaghouri@Kinans-MacBook-Air.local>
2018-11-28 18:37:06 -0500

    Final

---
+++ assets/javascript/app.js
@@ -0,0 +1,103 @@
+$(document).ready(function() {
+    // GLOBAL VARIABLES
+    // ===========================================================
+    var showGame = $("#visibleGame");
+    var showEndGame = $("#visibleEndGame");
+    var time = 30;
+    var intervalId;
+  
+    // COUNTERS
+    // ===========================================================
+    var correct = 0;
+    var incorrect = 0;
+    // var unanswered = 0;
+  
+    // BUTTON ACTIONS
+    // ===========================================================
+    $("#startGame").on("click", function() {
+      startGame();
+    })
+  
+    $("#doneGame").on("click", function() {
+      doneGame();
+    })
+  
+    // BEGIN GAME
+    // ===========================================================
+    function startGame() {
+      // Transition to questions element
+      $("#startGame").replaceWith(showGame);
+      // Start Timer
+      intervalId = setInterval(function() {
+        time--;
+        $("#timer").html("Time Remaining: " + time);
+        if (time == 0) {
+          doneGame();
+        }
+      }, 1000);
+    }
+  
+    // END GAME
+    // ===========================================================
+    function doneGame() {
+  
+      clearInterval(intervalId);
+  
+      if ($("#correct1").prop("checked")) {
+        correct++;
+      } else {
+        incorrect++;
+      }
+  
+      if ($("#correct2").prop("checked")) {
+        correct++;
+      } else {
+        incorrect++;
+      }
+  
+      if ($("#correct3").prop("checked")) {
+        correct++;
+      } else {
+        incorrect++;
+      }
+  
+      if ($("#correct4").prop("checked")) {
+        correct++;
+      } else {
+        incorrect++;
+      }
+  
+      if ($("#correct5").prop("checked")) {
+        correct++;
+      } else {
+        incorrect++;
+      }
+  
+      if ($("#correct6").prop("checked")) {
+        correct++;
+      } else {
+        incorrect++;
+      }
+  
+      if ($("#correct7").prop("checked")) {
+        correct++;
+      } else {
+        incorrect++;
+      }
+  
+      if ($("#correct8").prop("checked")) {
+        correct++;
+      } else {
+        incorrect++;
+      }
+  
+      // Transition to correct/incorrect element
+      $("#visibleGame").replaceWith(showEndGame);
+  
+      $("#correct").html(correct);
+      $("#incorrect").html(incorrect);
+    }
+  
+  
+  });
+  
